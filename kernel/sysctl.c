93293566e5886e191c4516cbf5484e0a150fbf37
Lingutla Chandrasekhar <clingutla@codeaurora.org>
2019-11-08 17:49:38 +0530

    sched: core: Use sched_clusters for updown migration handler
    Currently, sched updown migration handler derives cluster topology
    based on arch topology, the cluster information is already populated
    in walt sched_cluster. So reuse it instead of deriving it again.
    
    And move updown tunables support to under WALT.
    
    Change-Id: Iddf4d18ddf75cc20637281d9889f671f42369513
    Signed-off-by: Lingutla Chandrasekhar <clingutla@codeaurora.org>
    Signed-off-by: PrimoDev23 <lexx.ps2711@gmail.com>

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -426,7 +426,6 @@ static struct ctl_table kern_table[] = {
 		.extra1		= &zero,
 		.extra2		= &two_million,
 	},
-#endif
 	{
 		.procname	= "sched_upmigrate",
 		.data		= &sysctl_sched_capacity_margin_up,
@@ -441,6 +440,7 @@ static struct ctl_table kern_table[] = {
 		.mode		= 0644,
 		.proc_handler	= sched_updown_migrate_handler,
 	},
+#endif
 #ifdef CONFIG_SCHED_DEBUG
 	{
 		.procname	= "sched_min_granularity_ns",
