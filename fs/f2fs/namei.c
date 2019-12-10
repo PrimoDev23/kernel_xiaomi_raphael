7eeed962c0f169e1d82116e79c136663ae6bbf71
Jaegeuk Kim <jaegeuk@kernel.org>
2019-12-09 19:03:05 -0800

    f2fs: convert inline_dir early before starting rename
    If we hit an error during rename, we'll get two dentries in different
    directories.
    
    Chao adds to check the room in inline_dir which can avoid needless
    inversion. This should be done by inode_lock(&old_dir).
    
    Signed-off-by: Chao Yu <yuchao0@huawei.com>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>
    Signed-off-by: UtsavisGreat <utsavbalar1231@gmail.com>
    Signed-off-by: PrimoDev23 <lexx.ps2711@gmail.com>

--- fs/f2fs/namei.c
+++ fs/f2fs/namei.c
@@ -868,14 +868,15 @@ static int f2fs_rename(struct inode *old_dir, struct dentry *old_dentry,
 		return -EXDEV;
 
 	/*
-	 * old entry and new entry can locate in the same inline
-	 * dentry in inode, when attaching new entry in inline dentry,
-	 * it could force inline dentry conversion, after that,
-	 * old_entry and old_page will point to wrong address, in
-	 * order to avoid this, let's do the check and update here.
+	 * If new_inode is null, the below renaming flow will
+	 * add a link in old_dir which can conver inline_dir.
+	 * After then, if we failed to get the entry due to other
+	 * reasons like ENOMEM, we had to remove the new entry.
+	 * Instead of adding such the error handling routine, let's
+	 * simply convert first here.
 	 */
 	if (old_dir == new_dir && !new_inode) {
-		err = f2fs_convert_inline_dir(old_dir);
+		err = f2fs_try_convert_inline_dir(old_dir, new_dentry);
 		if (err)
 			return err;
 	}
