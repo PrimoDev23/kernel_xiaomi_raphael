182a32fe74fecb461cd9dd6a137142dcd7cb8727
PrimoDev23 <lexx.ps2711@gmail.com>
2019-12-24 01:24:40 +0100

    sched/fair: Partially apply https://github.com/PrimoDev23/kernel_xiaomi_raphael/commit/756e9948b19eadbe8090b236667ccdd194c7de51 again
    I messed this up when backporting stuff, so add this again

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -3523,7 +3523,7 @@ static inline void update_load_avg(struct sched_entity *se, int flags)
 	if (se->avg.last_update_time && !(flags & SKIP_AGE_LOAD))
 		__update_load_avg_se(now, cfs_rq, se);
 
-	decayed  = update_cfs_rq_load_avg(now, cfs_rq, true);
+	decayed  = update_cfs_rq_load_avg(now, cfs_rq, !(flags & SKIP_CPUFREQ));
 	decayed |= propagate_entity_load_avg(se);
 
 	if (!se->avg.last_update_time && (flags & DO_ATTACH)) {
